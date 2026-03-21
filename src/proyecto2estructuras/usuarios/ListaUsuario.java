88918af1e0531a25274f5ce1428cdb90a92f6550
PC <PC@LucianoRoaPc>
2026-03-21 13:48:15 -0400

    primitivas de ListaUsuario

--- src/proyecto2estructuras/usuarios/ListaUsuario.java
+++ src/proyecto2estructuras/usuarios/ListaUsuario.java
@@ -4,10 +4,71 @@
  */
 package proyecto2estructuras.usuarios;
 
+import proyecto2estructuras.documentos.Documento;
+import proyecto2estructuras.documentos.NodoListaD;
+
 /**
  *
  * @author PC
  */
 public class ListaUsuario {
+    NodoListaU primero = null;
+
+    public ListaUsuario() {
+    }
+    
+  
+    public void agregar(Usuario u) {
+        NodoListaU nuevo = new NodoListaU(u);
+        if (primero == null) {
+            primero = nuevo;
+        } else {
+            NodoListaU aux = primero;
+            while (aux.sig != null) {
+                aux = aux.sig;
+            }
+            aux.sig = nuevo;
+        }
+    }
+
+    
+    public void eliminar(String nombre) {
+        if (primero == null) return;
+
+        if (primero.dato.nombre.equals(nombre)) {
+            primero = primero.sig;
+            return;
+        }
+
+        NodoListaU anterior = primero;
+        NodoListaU aux = primero;
+        while (aux != null) {
+            if (aux.dato.nombre.equals(nombre)) {
+                anterior.sig = aux.sig;
+                return;
+            }
+            anterior = aux;
+            aux = aux.sig;
+        }
+    }
+
+    public NodoListaU buscar(String nombre) {
+        NodoListaU aux = primero;
+        while (aux != null) {
+            if (aux.dato.nombre.equals(nombre)) return aux;
+            aux = aux.sig;
+        }
+        return null;
+    }
+
+    public String imprimir() {
+        String otaku = " ";
+        NodoListaU aux = primero;
+        while (aux != null) {
+            otaku = aux.dato.nombre + " -> " + aux.dato.tipo + "->" + aux.dato.lDoc.imprimir();
+            aux = aux.sig;
+        }
+        return otaku;
+    }
     
 }
