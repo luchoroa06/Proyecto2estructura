1067dd4e31d925f9c9ea19d10afdb6b8cc605e04
PC <PC@LucianoRoaPc>
2026-03-21 13:07:51 -0400

    clase de Documentos y sus "Nodo()nombre, tamaño, tipo, prioridad, creador"

---
+++ src/proyecto2estructuras/documentos/Documento.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2estructuras.documentos;
+
+import proyecto2estructuras.usuarios.Usuario;
+
+/**
+ *
+ * @author PC
+ */
+public class Documento {
+    String nombre;  //nombre del documento
+    int tamaño;     //tamaño del documento
+    String tipo;    //tipo de docunto
+    String prioridad;  //prioridad del documento0
+    Usuario creador;      //usuario que creo el documento;
+    
+    /**
+     * contructor del documento y sus datos.
+     * @param nombre
+     * @param tamaño
+     * @param tipo
+     * @param creador 
+     */
+    public Documento(String nombre, int tamaño, String tipo, Usuario creador) {
+        this.nombre = nombre;
+        this.tamaño = tamaño;
+        this.tipo = tipo;
+        this.prioridad = null;
+        this.creador = creador;
+    }
+    
+    
+   
+}
