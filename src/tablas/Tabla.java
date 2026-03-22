6589f0a0722725bd8cf68ccd2b744583476c8dab
PC <PC@LucianoRoaPc>
2026-03-22 19:49:53 -0400

    Creacion de la clase hash y el agregar, buscar y eliminar en el array de tablas

--- src/tablas/Tabla.java
+++ src/tablas/Tabla.java
@@ -4,10 +4,54 @@
  */
 package tablas;
 
+import proyecto2estructuras.documentos.Documento;
+import proyecto2estructuras.usuarios.Usuario;
+
 /**
  *
  * @author sebas
  */
 public class Tabla {
+    ListaTabla[] tabla;   //creacion de la array del hashtable;
+    int t;
+/**
+ * contrustor del hastable
+ * @param t 
+ */
+    public Tabla(int t) {
+        this.t = t;
+        tabla = new ListaTabla[t];
+        for (int i = 0; i < t; i++) {
+            tabla[t] = new ListaTabla();
+        }
+    }
+    public int hash(String guillenteamo){                     //guillen te amo, sumebe nota porfis ☺
+        int hash = 0;     
+        for (int i = 0; i < guillenteamo.length(); i++) {       //recorremos el array
+           hash += guillenteamo.charAt(i)+73*hash;      //lo multiplicamos por un numero primo para sea mas certero
+        }
+        if(hash< 0){                // por si el hash da negativo
+            hash = hash*(-1);
+        }
+        return hash%t;      //retornamos el resto
+    }
     
+    public void agregar(Documento docu, Usuario id){   //agrega el documento a base del documento y usuario y llama insertar de Listatabla y la agrega
+        String clave = docu.nombre + id.nombre;
+        int resultado = this.hash(clave);
+        tabla[resultado].insertar(docu, id);
+    }
+    
+    public NodoTabla buscar(Documento docu, Usuario id){    //busca el documento a base del documento y usuario y llama buscar de Listatabla y retorna Nombre del documento 
+        String clave = docu.nombre + id.nombre;             //y el id de usuario
+        int resultado = this.hash(clave);
+        NodoTabla a = tabla[resultado].buscar(docu, id);
+        return a;
+    }
+    
+    public void eliminar(Documento docu, Usuario id){   //elimina el documento a base del documento y usuario y llama eliminar de Listatabla y lo elimina.
+        String clave = docu.nombre + id.nombre;
+        int resultado = this.hash(clave);
+        tabla[resultado].eliminar(docu, id);
+    }
 }
