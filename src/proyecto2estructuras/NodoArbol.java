a843c5511b1026296c5838d9cbf2905cac9e21ad
PC <PC@LucianoRoaPc>
2026-03-25 15:50:20 -0400

    Creacion del Nodo para el monticulo binario

---
+++ src/proyecto2estructuras/NodoArbol.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2estructuras;
+
+import proyecto2estructuras.documentos.Documento;
+
+/**
+ *
+ * @author PC
+ */
+public class NodoArbol {
+    public Documento documento;
+    public int etiquetaTiempo; 
+
+    public NodoArbol(Documento documento, int etiquetaTiempo) {
+        this.documento = documento;
+        this.etiquetaTiempo = etiquetaTiempo;
+    }
+}
+
+    
+
